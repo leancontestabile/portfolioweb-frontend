510a9f6345e5953fc12435988f700d34886b2799
lean <leancontestabile@gmail.com>
2023-03-26 20:59:31 -0300

    actualizacion firebase

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
 })
 export class SExperienciaService {
 
-  expURL = 'http://localhost:8080/explab/'
+  expURL = 'https://backendleancontestabile.onrender.com/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
